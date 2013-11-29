bfae7cb67dffc0a6b84cb3f0cf2d6ba913b16c56
Konrad Gądek <kgadek@gmail.com>
2013-11-29 01:09:35 +0100

    HAAAAAAAAAAAANDS

--- py/res.py
+++ py/res.py
@@ -1,32 +1,38 @@
 #!/usr/bin/env python3
 
 class Un:
-    def __init__(self, number, part=None):
+    def __init__(self, number=None, part=None):
         self.number = number
         self.part = part
     def __repr__(self):
+        number = self.number
+        if self.number is None:
+            number = "??"
         if self.part:
-            return "u{}_{}".format(self.number, self.part)
-        return "u{}".format(self.number)
+            return "u{}_{}".format(number, self.part)
+        return "u{}".format(number)
     def __add__(self, other):
-        if self.number != other.number or not self.part or not other.part or self.part == other.part:
+        if self.number and self.number and (self.number != other.number or not self.part or not other.part or self.part == other.part):
             raise Exception()
         return self.__class__(self.number)
 
 
 class Matrix:
-    def __init__(self, rows, cols, rowdescrs=None, coldescrs=None):
+    def __init__(self, rows, cols, rowdescrs=None, coldescrs=None, flatvals=None):
         self.rows = rows
         self.cols = cols 
         self.m = [ [0.0
                     for _j in range(cols)]
                   for _i in range(rows)]
         if not rowdescrs:
-            rowdescrs = [Un(i) for i in range(rows)]
+            rowdescrs = [Un() for _i in range(rows)]
         self.rowdescrs = rowdescrs
         if not coldescrs:
-            coldescrs = [Un(i) for i in range(rows)]
+            coldescrs = [Un() for _i in range(rows)]
         self.coldescrs = coldescrs
+        if flatvals:
+            for i, v in enumerate(flatvals):
+                self.m[i//cols][i%cols] = v
 
     def __repr__(self):
         res = "{0:>13}".format("| ")
@@ -68,3 +74,37 @@ class Matrix:
             res[rowId] = [x+y for x,y in zip(rowA, rowB)]
         return res
 
+    def cutpart(self, rows, cols):
+        res = self.__class__(len(rows), len(cols), [self.rowdescrs[row] for row in rows], [self.coldescrs[col] for col in cols])
+        for rowId, rowA in enumerate(rows):
+            res[rowId] = [self[rowA][c] for c in cols]
+        return res
+
+    @classmethod
+    def prodA1(cls, a, b):
+        res = cls.prodA2(a, b)
+        res.swapcol(0, 1)
+        res.swaprow(0, 1)
+        return res
+
+    @classmethod
+    def prodA2(cls, a, b):
+        coldescrs = [a.coldescrs[0], a.coldescrs[1] + b.coldescrs[0], b.coldescrs[1], ""]
+        rowdescrs = [a.rowdescrs[0], a.rowdescrs[1] + b.rowdescrs[0], b.rowdescrs[1], ""]
+        res = cls(3, 4, coldescrs, rowdescrs)
+        res[0] = a[0] + [a[0][2]]
+        res[1] = [a[1][0], a[1][1]+b[0][0], b[0][1], a[1][2]+b[0][2]]
+        res[2] = [0.0, b[1][0], b[1][1], b[1][2]]
+        return res
+
+
+if __name__ == "__main__":
+    a1 = Matrix(2, 3, [Un(0),    Un(1, 1)], [Un(0),    Un(1, 1), ""], [1, 0, 0, 1, -1, 0])
+    a2 = Matrix(2, 3, [Un(1, 2), Un(2, 1)], [Un(1, 2), Un(2, 1), ""],       [-1, 1, 0, 1, -1, 0])
+    print(a1)
+    print(a2)
+
+    b1 = Matrix.prodA1(a1,a2)
+    print(b1)
+    b1 = b1.cutpart([1,2], [1,2,3])
+    print(b1)
